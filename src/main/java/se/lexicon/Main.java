931e065f2a4921e0468f3329724c58bd96c0ec8f
MD90-Skola <micheldahl90@outlook.com>
2025-04-09 23:39:12 +0200

    If statment jämna tal

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,3 +1,5 @@
+
+
 package se.lexicon;
 
 import java.util.Scanner;
@@ -6,15 +8,104 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int nr = 50;
 
-        if(nr > 10){
-            System.out.println("nr är större än tio.");
-        } else if (nr <100) {
-            System.out.println("nr är mindre än hundra.");
+        // skriva ut alla gämna tal 1-10
 
 
+
+
+        /*
+        (int i =1; Första gången vi kör våran forloop då skapas I då får det värdet 1
+        i < 10   ; eftersom 1 är minde än 10 så komme den köra forloopen
+        i = i +1 ; sen plussar på så det blir 2
+
+          for (int i =1; i < 10;i = i +1 ){
+            System.out.println(i);
         }
+
+         */
+
+
+         //   modulo operator
+        //   % delar det värdet som står framför med det värdet som står bakom
+        //     och sen kollar den vad som blir över
+
+        for (int i = 1; i <= 10; i = i + 1) {
+            if (i% 2 == 0){   // kollar om tal är jämt
+                System.out.println(i);
+
+
+            }
+        }
+
+
+        //
+       //  System.out.println(8 % 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+        int nr = 50;
+        String name = "Frodo";
+
+
+        // 2st && = både vilkår 10 eller 100 måste stämma skriver jag 101 så kommer den inte skrva ut tex:
+        // går att blanda int med string
+
+        if (nr > 10 && name.equalsIgnoreCase("Frodo")) {
+            System.out.println("nr är större än 10 och mindre än 100");
+        }else if (nr>10){
+            System.out.println("nr är större än tio.");
+        }else if (nr<100){
+            System.out.println("nr är mindre än 100");
+        }
+*/
+
+
+
+
+
+
+
+/*
+
+                                        Denna kod kollar vilkår
+
+        // 2st && = både vilkår 10 eller 100 måste stämma skriver jag 101 så kommer den inte skrva ut tex:
+        if (nr > 10 && nr < 100) {
+            System.out.println("nr är större än 10 och mindre än 100");
+        }
+*/
+
+
+
+
+
+
+/*
+
+                                    // nu kommer den kolla om nummer är större än 10
+                                    //              ELLER
+                                    // om nummer är mindre än 100
+                                    //                OCH OM
+                                    //       något av dom här två stämmer
+
+        // 2st && = både vilkår 10 eller 100 måste stämma skriver jag 101 så kommer den inte skrva ut tex:
+        if (nr > 10 || nr < 100) {
+            System.out.println("nr är större än 10 och mindre än 100");
+        }
+*/
 
 
     }
