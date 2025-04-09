2bc20065f51951b9c15b8f960de5deadb838cc73
MD90-Skola <micheldahl90@outlook.com>
2025-04-09 23:04:15 +0200

    If statment

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -6,20 +6,14 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int nr = 50;
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("hej. skriv en månad");
+        if(nr > 10){
+            System.out.println("nr är större än tio.");
+        } else if (nr <100) {
+            System.out.println("nr är mindre än hundra.");
 
 
-        String input = sc.nextLine();
-        switch (input.toLowerCase()) {  // input.toLowerCase förvandlar alla till små bokstäver
-
-            // ska man ha in int eller double så ska man inte ha "täcken" för skriv bara siffran rätt ut
-            case "januari", "mars", "juli", "augusti", "oktober", "december" -> System.out.println("31");
-            case "april", "juni", "september", "november" -> System.out.println("30");
-            case "februari" -> System.out.println("28");
-
-            default -> System.out.println("Fel input");
         }
 
 
