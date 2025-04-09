4f86098ec82c560ba12fc4ab7bec2df9129e2b15
MD90-Skola <micheldahl90@outlook.com>
2025-04-09 21:17:48 +0200

    Sum for loops

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -5,10 +5,31 @@ package se.lexicon;
 public class Main {
     public static void main(String[] args) {
 
+
+        int sum = 0;
+
+        for(int i = 0; i <=10; i++){
+            sum=sum +i;
+
+            System.out.println("sum är"+sum);
+            System.out.println("-----");
+            System.out.println("I är"+i);
+        }
+
+        System.out.println(sum);
+
+
+
+
+
+
+
+/*
         for(int i = 1; i <= 10; i = i + 1){
             //kommer köras endast om villkoret stämmer
             System.out.println(i);
         }
+*/
 
 
 
