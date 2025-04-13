2a2255e7c328f9b470e5fbaedbd0b69fbf26d137
Michel Dahl <Michel.dahl90@gmail.com>
2025-04-13 14:37:05 +0200

    Update Main.java

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,16 +1,31 @@
 package se.lexicon;
+import java.util.Scanner;
+// https://www.youtube.com/watch?v=mMK6l5-nbFM Faceit
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Skriv in en in siffra 0-100");
+        int input = sc.nextInt();
 
-        for(int i = 1; i <= 10; i = i + 1){
-            //kommer köras endast om villkoret stämmer
-            System.out.println(i);
+
+        // om i är detlbart med 3, skriv ut Fizzz
+        // om i är delbart med 5, skriv ut Buzz
+        // om i är delbart med både 3 och 5, skriv ut FizzBuzz
+
+        for (int i = 1; i <= input; i = i + 1) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+
+
         }
-
-
-
     }
 }
